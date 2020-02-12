80bacaa6d8ad9327b2401c445c67e94e631ff107
kush11 <kush.codingmart.com>
2020-02-12 16:38:59 +0530

    Updates

---
+++ precache-manifest.ce5af02ee0fd7b20ce0b070fc9f00540.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1bbb650873e0b8f91d24fa2bdcd0b7",
+    "url": "/Exercise_Tracker/index.html"
+  },
+  {
+    "revision": "7bcbb5e1e8ad762d003b",
+    "url": "/Exercise_Tracker/static/css/2.ce2441d0.chunk.css"
+  },
+  {
+    "revision": "2b35645c9e3cd1015209",
+    "url": "/Exercise_Tracker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7bcbb5e1e8ad762d003b",
+    "url": "/Exercise_Tracker/static/js/2.9873ecb0.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/Exercise_Tracker/static/js/2.9873ecb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b35645c9e3cd1015209",
+    "url": "/Exercise_Tracker/static/js/main.42a1387c.chunk.js"
+  },
+  {
+    "revision": "d6f3e6b1758b9052c061",
+    "url": "/Exercise_Tracker/static/js/runtime-main.c31c395c.js"
+  }
+]);
